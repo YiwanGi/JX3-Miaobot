41d0622a0ac9a57785c4b346430bf6ceec0c2cb3
Wang <69124377+YiwanGi@users.noreply.github.com>
2021-06-17 01:58:47 +0800

    Add files via upload

---
+++ src/Custom/global_function.php
@@ -0,0 +1,7 @@
+<?php #plain
+# ┌-------------------------------------------------------------┐
+# ╎     Open Source: https://github.com/YiwanGi/JX3-Miaobot     ╎
+# ╎     MIT License: https://opensource.org/licenses/MIT        ╎
+# ╎ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ╎
+# ╎       Do what you want, but keep the agreement. (MIT)       ╎
+# └-------------------------------------------------------------┘
